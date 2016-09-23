5d9a7f5ca614c0f07c9393556ec76c1f3a947ac3
HEGGIE Alastair <s0822514@ed.ac.uk>
2016-09-23 17:50:05 +0100

    Changed mathjax to webtex

--- pan.ps1
+++ pan.ps1
@@ -4,7 +4,7 @@ cd .\processing
 
 gci -r -i *.md |foreach{(Get-Content $_).replace('.md', '.html') | Set-Content $_}
 
-gi *.md |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography=..\bibliography\bibliography.bib}
+gi *.md |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography=..\bibliography\bibliography.bib --webtex  }
 
 cd ..
 
