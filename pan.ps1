edbb6fc0619220c9e32727df30d677448228467f
AliHeggie <alastair.heggie@gmail.com>
2017-12-19 19:53:33 +0000

    update

--- pan.ps1
+++ pan.ps1
@@ -1,3 +1,4 @@
+
 Write-Host "Num Files:" $args.Length;
 
 if ($args.count[0]){
@@ -7,7 +8,7 @@ if ($args.count[0]){
     Copy-Item -force $arg .\processing
     cd .\processing
     (Get-Content $arg).replace('.md', '.html') | Set-Content $arg
-    gi $arg |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography=..\bibliography\bibliography.bib --webtex  }
+    gi $arg |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography=..\Bibliography\bibliography.bib --webtex  }
     cd ..
     Move-Item -force .\processing\*.html .\
     }
@@ -16,7 +17,7 @@ else{
     Copy-Item -force *.md .\processing
     cd .\processing
     gci -r -i *.md |foreach{(Get-Content $_).replace('.md', '.html') | Set-Content $_}
-    gi *.md |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography=..\bibliography\bibliography.bib --webtex  }
-    cd ..
+#    gi *.md |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography= $bibname --webtex  }
+    gi *.md |foreach{pandoc --filter pandoc-citeproc $_.name -f markdown -c ..\Stylesheets\github-pandoc.css --self-contained -t html -s -o  "$($_.basename).html" --bibliography=..\Bibliography\bibliography.bib --webtex  }    cd ..
     Move-Item -force .\processing\*.html .\
 }
